8f693fff3c6a999adbf8857b6364387b552b1a71
Peter Engelbert <pmengelbert@gmail.com>
2026-02-26 13:46:47 -0500

    Update doc comment
    Signed-off-by: Peter Engelbert <pmengelbert@gmail.com>

--- staging/src/k8s.io/kubectl/pkg/config/v1beta1/types.go
+++ staging/src/k8s.io/kubectl/pkg/config/v1beta1/types.go
@@ -122,6 +122,12 @@ type AllowlistEntry struct {
 	// either call to `exec.LookPath` results in an error, the `Name` check
 	// will be considered a failure.
 	Name string `json:"name"`
+	// Command matching is performed by first resolving the absolute path of both
+	// the plugin and the name in the allowlist entry using `exec.LookPath`. It
+	// will be called on both, and the resulting strings must be equal. If
+	// either call to `exec.LookPath` results in an error, the `Command` check
+	// will be considered a failure.
+	Command string `json:"command"`
 }
 
 // AliasOverride stores the alias definitions.
